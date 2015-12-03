d9c1d89e29a788a319d19c6e3b3de4a154a10077
bees <daneshv2@illinois.edu>
2015-12-02 20:49:18 -0600

    algo2 implemented, working

---
+++ src/droplet.cpp
@@ -0,0 +1,10 @@
+#include "droplet.h"
+
+
+Droplet::Droplet(int a, double v, double d){
+        amplitude = a;
+        velocity = v;
+        diameter = d;
+}
+
+
---
+++ src/droplet.h
@@ -0,0 +1,15 @@
+#ifndef DROPLET_H
+#define DROPLET_H
+
+class Droplet
+{
+    public:
+        Droplet(int a, double v, double d);
+
+        int amplitude;
+        double velocity;
+        double diameter;
+
+};
+
+#endif //DROPLET_H
